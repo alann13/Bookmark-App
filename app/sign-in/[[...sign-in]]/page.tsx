54b6e43815052b9336a02638c254e8e62a02f89d
alann13 <alan.nguy@outlook.com>
2025-12-29 19:20:25 -0500

    sign in widget now centered

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-64px)]">
+      <SignIn
+        appearance={{
+          variables: {
+            colorPrimary: '#014745',
+          },
+        }}
+      />
+    </div>
+  )
 }
